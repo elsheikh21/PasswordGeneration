d341a1b711125202e480271e9b469a2c77523f48
Ahmed ElSheikh <ahmedhelsheikh@gmail.com>
2019-02-16 18:39:36 +0200

    Init commit

---
+++ PasswordGeneration/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PasswordGeneration {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.GeneratePW = new System.Windows.Forms.Button();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GeneratePW
+            // 
+            this.GeneratePW.Location = new System.Drawing.Point(55, 126);
+            this.GeneratePW.Name = "GeneratePW";
+            this.GeneratePW.Size = new System.Drawing.Size(202, 29);
+            this.GeneratePW.TabIndex = 7;
+            this.GeneratePW.Text = "Generate and Reset";
+            this.GeneratePW.UseVisualStyleBackColor = true;
+            this.GeneratePW.Click += new System.EventHandler(this.GeneratePW_Click);
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.AccessibleName = "";
+            this.PasswordField.BackColor = System.Drawing.Color.White;
+            this.PasswordField.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordField.ForeColor = System.Drawing.Color.Black;
+            this.PasswordField.Location = new System.Drawing.Point(55, 60);
+            this.PasswordField.MaxLength = 15;
+            this.PasswordField.MinimumSize = new System.Drawing.Size(4, 40);
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.ReadOnly = true;
+            this.PasswordField.Size = new System.Drawing.Size(202, 22);
+            this.PasswordField.TabIndex = 8;
+            this.PasswordField.Text = "dasdsadasdasd";
+            this.PasswordField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.BackgroundImage = global::PasswordGeneration.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(334, 211);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.GeneratePW);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(350, 250);
+            this.MinimumSize = new System.Drawing.Size(350, 250);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Secure Me";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GeneratePW;
+        private System.Windows.Forms.TextBox PasswordField;
+    }
+}
+
---
+++ PasswordGeneration/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace PasswordGeneration {
+    public partial class MainForm :Form {
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        #region generate password
+        public static string PasswordGenerator() {
+            string Password = String.Empty;
+
+            return Password;
+        }
+        #endregion
+
+        #region Send email and reset server password
+        public void GeneratePW_Click(object sender, EventArgs e) {
+            string password = PasswordGenerator();
+            int ifDone = SendEmail( password );
+
+            if( ifDone == 1 ) {
+                MessageBox.Show( "Email is sent succesfully", "Secure Me notification" );
+                WriteLog( String.Format( "The email is successfully sent with your new password ({0})", password ) );
+            } else {
+                MessageBox.Show( "Email was not sent", "Secure Me notification" );
+                WriteLog( "The email was not send" );
+            }
+
+            // TODO: Check upon passwords reset
+            // get back to Hossam Barmawy
+        }
+        #endregion
+
+        #region Send an Email with the new password
+        public static int SendEmail(string password) {
+            try {
+                // Create the Outlook application.
+                Outlook.Application oApp = new Outlook.Application();
+
+                // Create a new mail item.
+                Outlook.MailItem oMsg = (Outlook.MailItem)oApp.CreateItem( Outlook.OlItemType.olMailItem );
+
+                // Set HTMLBody. 
+                //add the body of the email
+                oMsg.HTMLBody = String.Format( "Dear All, a new password was created upon your request ({0}) on ({1}).", password, GetTime() );
+
+                //Subject line
+                oMsg.Subject = "New password";
+
+                // Add a recipient.
+                Outlook.Recipients oRecips = (Outlook.Recipients)oMsg.Recipients;
+
+                // Change the recipient in the next line if necessary
+                string ToEmail = "hossam.elbarmawy@ecs-co.com";
+                Outlook.Recipient oRecip = (Outlook.Recipient)oRecips.Add( ToEmail );
+                oRecip.Resolve();
+
+                // Send.
+                ( (Outlook._MailItem)oMsg ).Send();
+                WriteLog( String.Format( "Email sent to ({0}) asking for new password ({1}) on ({2}) .", ToEmail, password, GetTime() ) );
+                System.Threading.Thread.Sleep( 1000 );
+
+                return 1;
+            } catch( Exception exc ) {
+                WriteLog( String.Format( "Error in email sending was due to: {0}", exc.ToString() ) );
+                return 0;
+            }
+        }
+        #endregion
+
+        #region Write log for successfull and unsuccessful events
+        public static void WriteLog(string text) {
+            string dirName = Path.GetDirectoryName( Assembly.GetExecutingAssembly().GetName().CodeBase ) + "\\log.txt";
+            string localPath = new Uri( dirName ).LocalPath;
+            using( StreamWriter sw = new StreamWriter( localPath, true ) ) {
+                // write to file at localPath ( log.txt )
+                sw.Write( Environment.NewLine + text + Environment.NewLine );
+            }
+        }
+        #endregion
+
+        #region Get current Date & Time
+        public static string GetTime() {
+            // return current date and time
+            return DateTime.Now.ToString();
+        }
+        #endregion
+
+    }
+}
+
+/*
+ * SendEmail(String Password) related 
+Add an attachment.
+String sDisplayName = "MyAttachment";
+int iPosition = (int)oMsg.Body.Length + 1;
+int iAttachType = (int)Outlook.OlAttachmentType.olByValue;
+now attached the file
+Outlook.Attachment oAttach = oMsg.Attachments.Add( @"C:\\fileName.jpg", iAttachType, iPosition, sDisplayName );
+*/
